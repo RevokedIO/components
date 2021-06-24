7c06777f893d65e050add727ab5186a2e71a0ef7
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-06-24 17:22:05 +0200

    ds

--- .ng-dev/release.ts
+++ .ng-dev/release.ts
@@ -1,23 +1,38 @@
 import {ReleaseConfig} from '@angular/dev-infra-private/release/config';
-import {releasePackages} from '../tools/release/release-output/release-packages';
+import {ReleaseAction} from '@angular/dev-infra-private/release/publish/actions';
+import {assertValidFrameworkPeerDependency} from '../tools/release-checks/check-framework-peer-dependency';
+import {SemVer} from 'semver';
+
+const actionProto = ReleaseAction.prototype as any;
+const _origStageFn = (actionProto).stageVersionForBranchAndCreatePullRequest;
+actionProto.stageVersionForBranchAndCreatePullRequest = async function(newVersion: SemVer) {
+  await assertValidFrameworkPeerDependency(newVersion);
+  console.error("OK");
+  await _origStageFn.apply(this, arguments);
+}
+
+/**
+ * Packages that will be published as part of the project. The order of packages
+ * here will control how sections appear in the changelog.
+ */
+export const releasePackagesInChangelogOrder = [
+  'cdk',
+  'material',
+  'google-maps',
+  'youtube-player',
+  'cdk-experimental',
+  'material-experimental',
+  'material-moment-adapter',
+];
 
 /** Configuration for the `ng-dev release` command. */
 export const release: ReleaseConfig = {
   releaseNotes: {
     useReleaseTitle: true,
-    // Hard-coded order for packages to show up in the changelog.
-    groupOrder: [
-      'cdk',
-      'material',
-      'google-maps',
-      'youtube-player',
-      'material-moment-adapter',
-      'cdk-experimental',
-      'material-experimental',
-    ]
+    groupOrder: releasePackagesInChangelogOrder
   },
   //publishRegistry: 'https://wombat-dressing-room.appspot.com',
-  npmPackages: releasePackages.map(pkg => `@angular/${pkg}`),
+  npmPackages: releasePackagesInChangelogOrder.map(pkg => `@angular/${pkg}`),
   buildPackages: async () => {
     // The performNpmReleaseBuild function is loaded at runtime as the loading the
     // script causes an invocation of bazel.
